6e6b8b4d0a655d98370a3f9f59c6c7ea76ce7698
Mara Dantas Pereira <maradantaspereira@gmail.com>
2026-09-23 14:58:03 -0300

    feat: adicionar bloco main em app.py para execução na porta 5001

--- app.py
+++ app.py
@@ -181,3 +181,6 @@ with app.app_context():
         ]
         db.session.add_all(categorias_iniciais)
         db.session.commit()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
